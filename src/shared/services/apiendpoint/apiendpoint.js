ceef87507b2ce6e2d2ada11dfa451b4e7ec3016e
vetri <vetrivel.tcz@gmail.com>
2025-03-07 19:12:44 +0530

    changes

--- src/shared/services/apiendpoint/apiendpoint.js
+++ src/shared/services/apiendpoint/apiendpoint.js
@@ -1,7 +1,7 @@
-const apiurl=()=>{
-    //  var url="http://192.168.29.240:3000";
-      // var url="http://192.168.29.175:3000";
-    var url="https://api.magizhkadai.com";
-    return url;
+const apiurl = () => {
+  //  var url="http://192.168.29.240:3000";
+  // var url="http://192.168.29.175:3000";
+  var url = "https://api.magizhkadai.com";
+  return url;
 }
 export default apiurl;
